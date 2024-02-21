05ee7818376b501e53da815d98c1094a2d259929
Rapchynskyi Oleksandr <rapchynskyioleksandr@gmail.com>
2024-02-22 00:46:13 +0200

    Added DB basic structure

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,51 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/RapchynskyiOleksandr/go-todo-list"
 	"github.com/RapchynskyiOleksandr/go-todo-list/pkg/handler"
 	"github.com/RapchynskyiOleksandr/go-todo-list/pkg/repository"
 	"github.com/RapchynskyiOleksandr/go-todo-list/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to init DB %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Errorwith running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
